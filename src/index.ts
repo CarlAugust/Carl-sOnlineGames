7422d4a7e37a7eee2db4869ebc5cf202694013df
DumbAugust <carlaugust.sponberg@gmail.com>
2025-03-05 09:38:33 +0100

    Form with a post

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,19 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req: Request, res: Response) => {
-  res.render('index');
+  const users: object = sql.getUsers();
+  res.render('index', { users: users });
+});
+
+app.post('/login', (req: Request, res: Response) => {
+  console.log(req.body);
+  res.send("Succ");
 });
 
 app.listen(port, () => {
